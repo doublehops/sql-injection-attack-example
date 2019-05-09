564fc4949206e1a073bc33e59a7d8b4fcfcf5d96
Damien Buttler <damien@doublehops.com>
2019-05-09 12:20:59 +1000

    Moved form to top of page.

--- web/sql-injection-playground.php
+++ web/sql-injection-playground.php
@@ -45,6 +45,22 @@
     <body>
         <h2>Insecure webform</h2>
         <p>This is a web form that that doesn't cleanse the variables in the backend. There are two users, `john` and `mary` with `password` being the password for both.</p>
+        
+        <h3>Login Form</h3>
+        <form method="POST" action="">
+
+            <?php if ($flash) : ?>
+               <div class="flash"><?= $flash ?></div>
+            <?php endif ?>
+
+            <dl>
+                <dt>Username</dt><dd><input type="text" name="username" value="<?= $_POST['username'] ?? '' ?>" /></dd>
+                <dt>Password</dt><dd><input type="text" name="password" value="<?= $_POST['password'] ?? '' ?>" id="password-input" /></dd>
+            </dl>
+            <input type="submit" name="submit" value="submit" />
+        </form>
+
+        <br />
 
         <div class="code-block">
             <h4>Backend code</h4>
@@ -58,50 +74,24 @@
         
         <h3>SQL Injection Examples</h3>
         <h4>Login without knowing username:</h4>
-        <p>Password: <em class="input">ddd') or 1=1;'</em></p>
+        <p>Password: <em class="input">xxxx') or 1=1;'</em></p>
         <div class="code-block">
             <h4>Resulting SQL</h4>
             <p>
-                SELECT * FROM user WHERE username='john' AND password=MD5('ddd') or 1=1;'')
+                SELECT * FROM user WHERE username='john' AND password=MD5('xxxx') or 1=1;'')
             </p>
         </div>
         <br />
 
         <h4>Make a change to the table</h4>
         <p>Change `last_name` of all users.</p>
-        <p>Password: <em class="input">ddd'); UPDATE user SET last_name='Jones';'</em></p>
+        <p>Password: <em class="input">xxxx'); UPDATE user SET last_name='Jones';'</em></p>
         <div class="code-block">
             <h4>Resulting SQL</h4>
             <p>
-                SELECT * FROM user WHERE username='damien' AND password=MD5('ddd'); UPDATE user SET last_name='Jones';'')
+                SELECT * FROM user WHERE username='john' AND password=MD5('somepassattempt'); UPDATE user SET last_name='Jones';'')
             </p>
         </div>
-
-        <?php if ($flash) : ?>
-           <div class="flash"><?= $flash ?></div>
-        <?php endif ?>
-        
-        <h3>Login Form</h3>
-        <form method="POST" action="">
-            <dl>
-                <dt>Username:</dt><dd><input type="text" name="username" value="<?= $_POST['username'] ?? '' ?>" /></dd>
-                <dt>Password:</dt><dd><input type="text" name="password" value="<?= $_POST['password'] ?? '' ?>" id="password-input" /></dd>
-            </dl>
-            <input type="submit" name="submit" value="submit" />
-        </form>
-
-        <h3><a href="?table=contact">Contact List</a></h3>
-        <?php if ($items) : ?>
-            <table>
-                <?php foreach ($items as $item) : ?>
-                <tr>
-                    <?php foreach ($item as $field) : ?>
-                    <td><?= $field ?></td>
-                    <?php endforeach ?>
-                </tr>
-                <?php endforeach ?>
-            </table>
-        <?php endif ?>
 
         <p class="bottom-note"><strong>NOTE: Do NOT use MD5 to hash passwords.</strong></p>
 
