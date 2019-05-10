f868dccba3f5fc021f1eabbb7ed96d23db68ada6
Damien Buttler <damien@doublehops.com>
2019-05-10 15:08:38 +1000

    More instructions.

--- web/index.php
+++ web/index.php
@@ -19,9 +19,16 @@ td, th {border: 1px solid #666; font-size: 75%; vertical-align: baseline; paddin
     <tr><th>PHP:</th><td><?= phpversion() ?></td></tr>
 </table>
 
-<p><a href="sql-injection-playground.php">SQL Injection Playground</a></p>
-<p><a href="contact-list.php">Contact List</a></p>
-<p><a href="login.php">Login</a></p>
+<p>
+    This project is to give some examples of how SQL Injection can be used on insecure code. There examples broken down
+    into different pages with the login page an example of gaining shell access to the server.
+</p>
+
+<ul>
+    <li><a href="sql-injection-playground.php">SQL Injection Playground</a></li>
+    <li><a href="contact-list.php">Contact List</a></li>
+    <li><a href="login.php">Login</a></li>
+</ul>
 
 
 <?php
